58004479c7470a4f360d0ba5b1826c529e2356bb
Matthew Holloway <matthew@staff.ietf.org>
2025-09-11 12:01:22 +1200

    chore: adding api access tokens

--- rfc-html-extractor/src/redClientGet.ts
+++ rfc-html-extractor/src/redClientGet.ts
@@ -1,35 +1,50 @@
-import { ApiClient } from "../../client/generated/red-client.ts";
-import { parseRfcStatusSlug } from "../../client/app/utilities/rfc-converter-status.ts";
-import { blankRfcCommon } from "./rfc.ts";
-import type { Rfc } from "../../client/generated/red-client.ts";
-import type { RfcCommon } from "../../client/app/utilities/rfc-validators.ts";
+import { ApiClient } from '../../client/generated/red-client.ts'
+import { parseRfcStatusSlug } from '../../client/app/utilities/rfc-converter-status.ts'
+import { blankRfcCommon } from './rfc.ts'
+import type { Rfc } from '../../client/generated/red-client.ts'
+import type { RfcCommon } from '../../client/app/utilities/rfc-validators.ts'
+import { assertIsString } from './utilities/typescript.ts'
 
-const redApiBase = process.env.RED_API_BASE ?? 'http://datatracker-rpc.datatracker.svc/'
+const redApiBase =
+  process.env.RED_API_BASE ?? 'http://datatracker-rpc.datatracker.svc/'
 
 export const getRedClient = () => {
   console.log('getting api client with base', redApiBase)
+
+  const NUXT_PUBLIC_DATATRACKER_BASE = process.env.NUXT_PUBLIC_DATATRACKER_BASE
+  const NUXT_CF_SERVICE_TOKEN_ID = process.env.NUXT_CF_SERVICE_TOKEN_ID
+  const NUXT_CF_SERVICE_TOKEN_SECRET = process.env.NUXT_CF_SERVICE_TOKEN_SECRET
+
+  assertIsString(NUXT_PUBLIC_DATATRACKER_BASE, "datatracker base wasn't a string")
+  assertIsString(NUXT_CF_SERVICE_TOKEN_ID, "cloudflare token wasn't a string")
+  assertIsString(NUXT_CF_SERVICE_TOKEN_SECRET, "cloudflare secret wasn't a string")
+
+  const headers: ApiClient['Config']['headers'] = {
+    'CF-Access-Client-Id':  NUXT_CF_SERVICE_TOKEN_ID,
+    'CF-Access-Client-Secret': NUXT_CF_SERVICE_TOKEN_SECRET,
+  }
+
   return new ApiClient({
-    baseUrl: redApiBase,
-  });
-};
+    baseUrl: NUXT_PUBLIC_DATATRACKER_BASE,
+    headers
+  })
+}
 
 export const getRfcCommon = async (rfcNumber: number): Promise<RfcCommon> => {
-  console.log("Getting API client")
+  console.log('Getting API client')
   const api = getRedClient()
-  console.log("Got API client", api)
-  console.log("docRetrive", rfcNumber)
+  console.log('Got API client', api)
+  console.log('docRetrive', rfcNumber)
   try {
     const rfc = await api.red.docRetrieve(rfcNumber)
-    console.log("after docRetrieve", api)
+    console.log('after docRetrieve', api)
     const rfcCommon = rfcToRfcCommon(rfc)
-    console.log("after docRetrieve common", api)
+    console.log('after docRetrieve common', api)
     return rfcCommon
   } catch (e) {
-    console.error(
-      "docRetrive catch()", e
-    )
+    console.error('docRetrive catch()', e)
     throw e
-  }  
+  }
 }
 
 export const rfcToRfcCommon = (rfc: Rfc): RfcCommon => {
--- rfc-html-extractor/src/utilities/typescript.ts
+++ rfc-html-extractor/src/utilities/typescript.ts
@@ -1,3 +1,9 @@
 export const assertNever = (value: never) => {
   throw new Error('Unexpected value: ' + value)
 }
+
+export function assertIsString(val: unknown, errorMessage: string): asserts val is string {
+  if (typeof val !== 'string') {
+    throw new Error(`Not a string typeof=${typeof val}. ${errorMessage}`)
+  }
+}
