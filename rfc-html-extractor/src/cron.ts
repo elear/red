4cdf876da1599ec9a9a1629106fab42169bb60e6
Matthew Holloway <matthew@staff.ietf.org>
2025-09-12 11:46:27 +1200

    chore: precomputing homepage latest rfcs

--- rfc-html-extractor/src/cron.ts
+++ rfc-html-extractor/src/cron.ts
@@ -4,7 +4,7 @@ import { getAllRFCs, getRedClient } from './redClientGet.ts'
 export const processCron = async (): Promise<void> => {
   console.log('Triggered by a cron job')
   const api = getRedClient()
-  const rfcs = await getAllRFCs({ api })
+  const allRfcs = await getAllRFCs({ api })
 
-  await generateHomepageLatest(rfcs)
+  await generateHomepageLatest(allRfcs)
 }
