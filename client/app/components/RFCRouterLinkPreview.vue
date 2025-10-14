ccecf4623cb07492f34709282c9376943b345f70
Matthew Holloway <matthew@staff.ietf.org>
2025-10-14 21:35:37 +1300

    fix: rfc link preview formatting fixes

--- client/app/components/RFCRouterLinkPreview.vue
+++ client/app/components/RFCRouterLinkPreview.vue
@@ -1,29 +1,55 @@
 <template>
-  <GraphicsIETFMotif class="absolute text-black w-[110px] h-[100px] right-0 top-0 print:hidden" :opacity="0.04" />
+  <GraphicsIETFMotif
+    class="absolute text-black w-[110px] h-[100px] right-0 top-0 print:hidden"
+    :opacity="0.04"
+  />
   <p class="text-base text-blue-900 dark:text-white font-bold leading-5">
     {{ props.rfcJson.title }}
   </p>
   <div class="pt-1">
-    <Tag size="small" :text="tagText" />
+    <Tag
+      size="small"
+      :text="tagText"
+    />
   </div>
   <p class="leading-5 pt-2 text-xs text-pretty">
     {{ props.rfcJson.abstract }}
   </p>
-  <ul v-if="list1" class="text-base text-blue-900 dark:text-white">
-    <li v-for="(part, index) in list1" :key="index" class="inline">
+  <ul
+    v-if="list1"
+    class="text-base text-blue-900 dark:text-white"
+  >
+    <li
+      v-for="(part, index) in list1"
+      :key="index"
+      class="inline"
+    >
       <GraphicsDiamond v-if="index > 0" />{{ part }}
     </li>
   </ul>
-  <ul v-if="list2" class="text-base text-gray-800 mt-1 dark:text-white">
-    <li v-for="(part, index) in list2" :key="index" class="inline">
-      <GraphicsDiamond v-if="index > 0" class="align-middle" />{{ part }}
+  <ul
+    v-if="list2"
+    class="text-base text-gray-800 mt-1 dark:text-white"
+  >
+    <li
+      v-for="(part, index) in list2"
+      :key="index"
+      class="inline"
+    >
+      <GraphicsDiamond
+        v-if="index > 0"
+        class="align-middle"
+      />{{ part }}
     </li>
   </ul>
-  <p v-if="obsoletedBy" :class="[
-    'text-red-700 dark:text-red-300 text-base print:text-black',
-    { 'mt-2': isMobileAbstractOpen }
-  ]">
-    <Renderable :val="obsoletedBy" />
+  <p
+    v-if="obsoletedBy"
+    :class="[
+      'text-red-700 dark:text-red-300 text-base print:text-black',
+      { 'mt-2': isMobileAbstractOpen }
+    ]"
+  >
+    <component :is="obsoletedBy" />
   </p>
 
   <div class="inline-block border-black dark:border-gray-700 border-t-1 w-1/2 min-w-3xs mt-3 mb-3"></div>
@@ -35,8 +61,10 @@
   </p>
 
   <p class="clear-both text-right mt-6 mb-10">
-    <A :href="rfcPathBuilder(props.rfcJson.doc_id)"
-      class="flex-inline rounded no-underline hover:underline focus:underline justify-center items-center bg-gray-100 dark:bg-gray-700 text-blue-400 dark:text-white px-4 pt-3 pb-4 mr-6">
+    <A
+      :href="rfcPathBuilder(props.rfcJson.doc_id)"
+      class="flex-inline rounded no-underline hover:underline focus:underline justify-center items-center bg-gray-100 dark:bg-gray-700 text-blue-400 dark:text-white px-4 pt-3 pb-4 mr-6"
+    >
       <component :is="formattedTitle" />
       <GraphicsChevron class="ml-2 inline -rotate-90" />
     </A>
@@ -90,8 +118,10 @@ function formatDate(isoDate: string | undefined): string | undefined {
 }
 
 function formatObsoletedBy(
-  obsoletedBy: RFCJSON['obsoleted_by']
+  rfcJson: RFCJSON
 ): (() => VNode) | undefined {
+  const { obsoleted_by: obsoletedBy } = rfcJson
+
   if (!obsoletedBy || obsoletedBy.length === 0) {
     return undefined
   }
@@ -109,7 +139,7 @@ function formatObsoletedBy(
             {
               href: infoRfcPathBuilder(obsoletedByItem),
               title: `${formatTitlePlaintext(obsoletedByItem)}`,
-              class: 'relative underline p-1 -m-1 hover:bg-gray-100'
+              class: 'relative underline p-1 -m-1 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-red-200'
             },
             [h('b', obsoletedByItem)]
           )
@@ -117,7 +147,7 @@ function formatObsoletedBy(
 
         return acc
       },
-      ['Obsoleted by '] as (string | VNode)[]
+      [`${rfcJson.doc_id} obsoleted by `] as (string | VNode)[]
     )
   )
 }
@@ -125,7 +155,7 @@ function formatObsoletedBy(
 const formattedTitle = computed(() => formatTitleAsVNode(props.rfcJson.doc_id))
 
 const obsoletedBy = computed(() =>
-  formatObsoletedBy(props.rfcJson.obsoleted_by)
+  formatObsoletedBy(props.rfcJson)
 )
 
 const list1 = computed(() => [
