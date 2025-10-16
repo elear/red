2bb70a392785f79a61d0b03b09909e01503c33ab
Matthew Holloway <matthew@staff.ietf.org>
2025-10-17 07:16:20 +1300

    feat: reduce text size for last updated

--- client/app/components/ContentDocModifiedDateTime.vue
+++ client/app/components/ContentDocModifiedDateTime.vue
@@ -2,14 +2,18 @@
   <HoverCardRoot v-model:open="isHoverCardOpen">
     <HoverCardTrigger as-child>
       <button
-        class="mt-10 px-4 -ml-2 py-2 italic border border-transparent focus:border focus:border-gray-400 rounded-md"
-        @focus="isHoverCardOpen = true" @mouseover="isHoverCardOpen = true" @blur="isHoverCardOpen = false">
+        class="mt-10 px-4 -ml-2 py-2 text-sm italic border border-transparent focus:border focus:border-gray-400 rounded-md"
+        @focus="isHoverCardOpen = true"
+        @mouseover="isHoverCardOpen = true"
+        @blur="isHoverCardOpen = false"
+      >
         Last updated {{ relativeDate }}
       </button>
     </HoverCardTrigger>
     <HoverCardPortal>
       <HoverCardContent
-        class="border shadow-2xl overflow-x-hidden rounded-md bg-white dark:bg-black border-gray-400 dark:border-white px-3 py-2 data-[side=bottom]:animate-slideUpAndFade data-[side=right]:animate-slideLeftAndFade data-[side=left]:animate-slideRightAndFade data-[side=top]:animate-slideDownAndFade data-[state=open]:transition-all">
+        class="border shadow-2xl overflow-x-hidden rounded-md bg-white dark:bg-black border-gray-400 dark:border-white px-3 py-2 data-[side=bottom]:animate-slideUpAndFade data-[side=right]:animate-slideLeftAndFade data-[side=left]:animate-slideRightAndFade data-[side=top]:animate-slideDownAndFade data-[state=open]:transition-all"
+      >
         <p class="italic font-bold">Last updated {{ fullDate }}</p>
         <HoverCardArrow class="fill-gray-200 stroke-gray-500 -mt-[1px]" />
       </HoverCardContent>
