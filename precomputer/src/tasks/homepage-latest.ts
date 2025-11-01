aeab8d0d5b8e074b30e18ab73e62fbbcc51d2630
Matthew Holloway <matthew@staff.ietf.org>
2025-11-02 03:20:48 +1300

    chore: precomputer homepage latest constants

--- precomputer/src/tasks/homepage-latest.ts
+++ precomputer/src/tasks/homepage-latest.ts
@@ -5,6 +5,8 @@ import type { RfcCommon } from '../../../website/app/utilities/rfc-validators.ts
 import { validateDocument } from '../utilities/validate-zod.ts'
 import { uploadRfcData } from './rfc.ts'
 
+const NUMBER_OF_LATEST_RFCS_ON_HOMEPAGE = 3
+
 export const uploadHomepageLatest = async (
   allRfcs: Readonly<RfcCommon[]>
 ): Promise<boolean> => {
@@ -12,9 +14,9 @@ export const uploadHomepageLatest = async (
   await saveToS3(HOMEPAGE_LATEST_PATH, JSON.stringify(data))
   console.log('Uploaded', HOMEPAGE_LATEST_PATH)
 
-  // also upload the 'homepage latest' RFCs so that the links to RFCs will work
-  await Promise.all(data.homepageLatest.map(rfc => uploadRfcData(rfc.number)))
-  
+  // also upload the referenced 'homepage latest' RFCs so that the links to RFCs will work
+  await Promise.all(data.homepageLatest.map((rfc) => uploadRfcData(rfc.number)))
+
   return true
 }
 
@@ -24,7 +26,9 @@ export const renderHomepageLatest = async (
   allRfcs: Readonly<RfcCommon[]>
 ): Promise<HomepageLatest> => {
   const response = {
-    homepageLatest: allRfcs.slice(-3).sort((a, b) => b.number - a.number)
+    homepageLatest: allRfcs
+      .slice(-NUMBER_OF_LATEST_RFCS_ON_HOMEPAGE)
+      .sort((a, b) => b.number - a.number)
   }
   validateDocument(response, HomepageLatestSchema)
   return response
