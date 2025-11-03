d22667e0215d5a4af2972ecaab46d31ae7ce5098
Matthew Holloway <matthew@staff.ietf.org>
2025-11-03 16:09:25 +1300

    chore: drop back to 8 concurrent rfc processes

--- precomputer/src/all.ts
+++ precomputer/src/all.ts
@@ -2,20 +2,29 @@ import { PromisePool } from '@supercharge/promise-pool'
 import { range } from 'lodash-es'
 import { uploadRfcData } from './tasks/rfc.ts'
 
-const NUMBER_OF_CONCURRENT_RFC_PROCESSORS = 16
+const NUMBER_OF_CONCURRENT_RFC_PROCESSORS = 8
 
 const main = async (
   minRfcNumber: number,
   maxRfcNumber: number
 ): Promise<void> => {
+  if (Number.isNaN(minRfcNumber)) {
+    throw Error(`Min RFC number (${minRfcNumber}) must be a number but was NaN`)
+  }
+  if (Number.isNaN(maxRfcNumber)) {
+    throw Error(`Max RFC number (${maxRfcNumber}) must be a number but was NaN`)
+  }
   if (minRfcNumber >= maxRfcNumber) {
     throw Error(
       `Min RFC number (${minRfcNumber}) must be smaller than max RFC number (${maxRfcNumber})`
     )
   }
+
   const rfcRange = range(minRfcNumber, maxRfcNumber)
 
-  console.log(`Processing ${minRfcNumber}-${maxRfcNumber}. Using ${NUMBER_OF_CONCURRENT_RFC_PROCESSORS} concurrent promises (results will appear out of order)`)
+  console.log(
+    `Processing ${minRfcNumber}-${maxRfcNumber}. Using ${NUMBER_OF_CONCURRENT_RFC_PROCESSORS} concurrent promises (results will appear out of order)`
+  )
   const { errors } = await PromisePool.for(rfcRange)
     .withConcurrency(NUMBER_OF_CONCURRENT_RFC_PROCESSORS)
     .process(async (rfcNumber) => {
@@ -24,7 +33,9 @@ const main = async (
         if (isDone) {
           console.log(`[RFC ${rfcNumber}] upload succeeded`)
         } else {
-          console.error(`[RFC ${rfcNumber}] generation failed. If the RFC was NOT_ISSUED this isn't an error.`)
+          console.error(
+            `[RFC ${rfcNumber}] generation failed. If the RFC was NOT_ISSUED this isn't an error.`
+          )
         }
       } catch (err) {
         console.warn(
@@ -34,13 +45,13 @@ const main = async (
       }
     })
 
-    if(errors.length > 0) {
-      console.log("all.ts finished with error(s)")
-      console.error(errors)
-      process.exit(1)
-    } else {
-      process.exit(0)
-    }
+  if (errors.length > 0) {
+    console.log('all.ts finished with error(s)')
+    console.error(errors)
+    process.exit(1)
+  } else {
+    process.exit(0)
+  }
 }
 
 if (!process.argv[2] || !process.argv[3]) {
