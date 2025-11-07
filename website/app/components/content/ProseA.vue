9051e9e71b5f37321a10a7983ec730324bd678f6
Matthew Holloway <matthew@staff.ietf.org>
2025-11-08 05:11:35 +1300

    feat: external (offiste) links in markdown are given icon

--- website/app/components/content/ProseA.vue
+++ website/app/components/content/ProseA.vue
@@ -5,11 +5,22 @@
     :class="ANCHOR_TAILWIND_STYLE"
   >
     <slot />
+    <Icon
+      v-if="!isInternal && !isMailTo && !isHash"
+      name="fluent:window-new-20-regular"
+      class="text-lg align-middle ml-1"
+    />
   </AMaybeRFCLink>
 </template>
 
 <script setup lang="ts">
 import { ANCHOR_TAILWIND_STYLE } from '~/utilities/theme'
+import { isHashLink, isInternalLink, isMailToLink } from '~/utilities/url';
 
 const props = defineProps<{ href?: string; id?: string }>()
+
+const isInternal = computed(() => isInternalLink(props.href))
+const isMailTo = computed(() => isMailToLink(props.href))
+const isHash = computed(() => isHashLink(props.href))
+
 </script>
