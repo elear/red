5fe3c2ea1830dfdc8414b871be7071a802ead027
Matthew Holloway <matthew@staff.ietf.org>
2025-11-13 19:35:28 +1300

    fix: search mobile

--- website/app/components/SearchMainHeader.vue
+++ website/app/components/SearchMainHeader.vue
@@ -1,21 +1,23 @@
 <template>
-    <Heading
-        level="1"
-        style-level="1"
-        class="bg-blue-900 text-white px-2 py-1 text-balance"
-    >
-        Search RFCs
-    </Heading>
-    <div class="sticky top-0 z-50 bg-blue-900 text-white mt-0 container mx-auto">
-        <div class="flex flex-row items-center pt-4 pb-4 ml-2">
+    <div class="bg-blue-900 text-white ">
+        <Heading
+            level="1"
+            style-level="1"
+            class="container mx-auto px-2 py-1 text-balance"
+        >
+            Search RFCs
+        </Heading>
+    </div>
+    <div class="lg:sticky lg:top-0 md:z-50 bg-blue-900 text-white mt-0">
+        <div class="flex flex-row items-center pt-4 pb-4 container mx-auto">
             <div class="w-full md:w-2/3">
                 <ais-search-box
                     autofocus
                     placeholder="Find an RFC (number, subseries, title, author, etc.)"
                     :class-names="{
-                        'ais-SearchBox-form': 'w-full flex',
+                        'ais-SearchBox-form': 'w-full flex ml-1',
                         'ais-SearchBox-input':
-                            aisSearchboxInputClass,
+                            'flex-1 min-w-0 bg-white text-black dark:bg-black dark:text-white dark:border-white dark:border pl-4 py-3 pr-2 h-12 rounded-l-xs',
                         'ais-SearchBox-submit':
                             'bg-blue-200 px-2 flex items-center rounded-r-xs',
                         'ais-SearchBox-reset': 'hidden',
@@ -58,6 +60,4 @@
 
 <script setup lang="ts">
 import { AisSearchBox } from 'vue-instantsearch/vue3/es'
-
-const aisSearchboxInputClass = 'flex-1 min-w-0 bg-white text-black dark:bg-black dark:text-white dark:border-white dark:border pl-4 py-3 pr-2 h-12 rounded-l-xs'
 </script>
