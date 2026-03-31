780cc9280316d55360b987b2c5395ba466c641ba
NGPixel <github@ngpixel.com>
2026-03-31 16:52:24 -0400

    ci(worker): transform routes at deploy time

---
+++ worker/deploy-utils/replace-routes.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ -z "$GITHUB_ACTION" ]; then
+  echo "DO NOT RUN THIS SCRIPT IN DEV. THIS IS FOR DEPLOYMENT ONLY."
+  exit 1
+fi
+
+fx wrangler.jsonc 'x => {
+  for (const envr in x.env) {
+    if (envr === "production") {
+      x.env.production.routes = x.env.staging.routes.map(r => ({
+        ...r,
+        pattern: r.pattern.replace(/staging\./, "")
+      }))
+    } else {
+      x.env[envr].routes = x.env.staging.routes.map(r => ({
+        ...r,
+        pattern: r.pattern.replace(/staging/, envr)
+      }))
+    }
+  }
+  return x
+}' save
