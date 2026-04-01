9cada79dfc2087923c7e6250ccd6ef29a5a0ce4c
Matthew Holloway <matthew@staff.ietf.org>
2026-04-01 20:43:26 +1300

    chore: adding debug for website origin

--- precomputer/src/tasks/indices.ts
+++ precomputer/src/tasks/indices.ts
@@ -12,6 +12,7 @@ import { uploadMetaThumbnails } from '../utilities/meta-thumbnails.ts'
 import { uploadFavicons } from '../utilities/favicons.ts'
 import { uploadRobotsTxtEtc } from '../utilities/robots-txt-etc.ts'
 import { PUBLIC_SITE_URL_ORIGIN, STAGING_SITE_URL_ORIGIN } from '../utilities/url.ts'
+import { assertIsString } from '../utilities/typescript.ts'
 
 const RFC_NUMBER_MINIMUM_CHAR_WIDTH = 5 // for Red the default width is 5 chars to handle eg RFC10000 (aka the RFC10k problem).
 
@@ -20,15 +21,14 @@ type Props = {
 }
 
 export const indices = async ({ api, }: Props): Promise<boolean> => {
-  console.log("Generating indices", process.env.S3_RED_BUCKET, Object.keys(process.env))
+  console.log("Generating indices for ", process.env.NUXT_PUBLIC_SITE_BASE)
   const [allRfcs, allSubseries] = await Promise.all([
     getAllRFCs({ api }),
     getAllSubseries({ api })
   ])
+  const websiteOrigin = process.env.NUXT_PUBLIC_SITE_BASE
 
-  const websiteOrigin = process.env.S3_RED_BUCKET?.includes('staging') ? STAGING_SITE_URL_ORIGIN : PUBLIC_SITE_URL_ORIGIN
-
-  console.log("Using website origin", websiteOrigin)
+  assertIsString(websiteOrigin, 'Expected process.env.NUXT_PUBLIC_SITE_BASE to be string')
 
   const results = await Promise.all([
     uploadHomepageLatest(allRfcs),
