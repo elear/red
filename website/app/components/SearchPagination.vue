02341b41a429db75d37e26379e1dcc9379e95f07
Matthew Holloway <matthew@staff.ietf.org>
2026-04-22 15:51:34 +1200

    fix: accessibility keyboard nav of search pagination

--- website/app/components/SearchPagination.vue
+++ website/app/components/SearchPagination.vue
@@ -6,33 +6,131 @@
     >
       <ais-pagination
         :class-names="{
-          'ais-Pagination': 'w-full md:w-auto',
-          'ais-Pagination-list': 'w-auto ml-auto mr-auto flex flex-row justify-left md:justify-center',
-          'ais-Pagination-item': 'mr-1 bg-gray-200 dark:bg-gray-900 rounded-xs',
+          'ais-Pagination': '',
+          'ais-Pagination-list': '',
+          'ais-Pagination-item': '',
           'ais-Pagination-item--selected':
-            'bg-gray-700 dark:bg-blue-200! text-white',
+            '',
           'ais-Pagination-item--disabled':
             'bg-transparent dark:bg-transparent text-gray-800 dark:text-gray-200',
           'ais-Pagination-link':
             'cursor-pointer py-2 px-3 block no-underline hover:underline focus:underline'
         }"
         @click="scrollUpToNewSearchResults"
-      />
+      >
+        <template v-slot="{
+          currentRefinement,
+          nbPages,
+          pages,
+          isFirstPage,
+          isLastPage,
+          refine,
+          createURL,
+          classNames,
+        }">
+          <div class="w-full md:w-auto">
+            {{ classNames }}
+            <ul class="w-auto ml-auto mr-auto flex flex-row justify-left md:justify-center">
+              <li
+                v-if="!isFirstPage"
+                class="mr-1 bg-gray-200 dark:bg-gray-900 rounded-xs"
+              >
+                <button
+                  type="button"
+                  aria-label="Go to start of search results"
+                  :class="{
+                    'cursor-pointer py-2 px-3 block no-underline hover:underline focus:underline ': true,
+                    'bg-gray-700 dark:bg-blue-200! text-white': isFirstPage,
+                    'mr-1 bg-gray-200 dark:bg-gray-900 rounded-xs': !isFirstPage,
+                  }"
+                  @click.prevent="refine(0)"
+                >
+                  ‹‹
+                </button>
+              </li>
+              <li v-if="!isFirstPage">
+                <button
+                  type="button"
+                  aria-label="Go back a page of search results"
+                  :class="{
+                    'cursor-pointer py-2 px-3 block no-underline hover:underline focus:underline ': true,
+                    'bg-gray-700 dark:bg-blue-200! text-white': isFirstPage,
+                    'mr-1 bg-gray-200 dark:bg-gray-900 rounded-xs': !isFirstPage,
+                  }"
+                  @click.prevent="refine(currentRefinement - 1)"
+                >
+                  ‹
+                </button>
+              </li>
+              <li
+                v-for="page in pages"
+                :key="page"
+              >
+                <button
+                  type="button"
+                  :class="{
+                    'cursor-pointer py-2 px-3 block no-underline hover:underline focus:underline ': true,
+                    'bg-gray-700 dark:bg-blue-200! text-white': page === currentRefinement,
+                    'mr-1 bg-gray-200 dark:bg-gray-900 rounded-xs': page !== currentRefinement,
+                  }"
+                  @click.prevent="refine(page)"
+                >
+                  {{ page + 1 }}
+                </button>
+              </li>
+              <li v-if="!isLastPage">
+                <button
+                  type="button"
+                  aria-label="Go to next page of search results"
+                  :class="{
+                    'cursor-pointer py-2 px-3 block no-underline hover:underline focus:underline ': true,
+                    'bg-gray-700 dark:bg-blue-200! text-white': isLastPage,
+                    'mr-1 bg-gray-200 dark:bg-gray-900 rounded-xs': !isLastPage,
+                  }"
+                  @click.prevent="refine(currentRefinement + 1)"
+                >
+                  ›
+                </button>
+              </li>
+              <li v-if="!isLastPage">
+                <button
+                  type="button"
+                  aria-label="Go to last page of search results"
+                  :class="{
+                    'cursor-pointer py-2 px-3 block no-underline hover:underline focus:underline ': true,
+                    'bg-gray-700 dark:bg-blue-200! text-white': isLastPage,
+                    'mr-1 bg-gray-200 dark:bg-gray-900 rounded-xs': !isLastPage,
+                  }"
+                  @click.prevent="refine(nbPages)"
+                >
+                  ››
+                </button>
+              </li>
+            </ul>
+          </div>
+        </template>
+      </ais-pagination>
     </HorizontalScrollable>
-    <ais-hits-per-page
-      :items="[
-        { label: '10 per page', value: 10, default: true },
-        { label: '25 per page', value: 25 },
-        { label: '50 per page', value: 50 },
-        { label: '100 per page', value: 100 }
-      ]"
-      :class-names="{
-        'ais-HitsPerPage':
-          'mt-6 md:mt-0 md:ml-4 px-2 bg-white text-base border border-gray-400 hover:border-black dark:bg-black dark:border-white dark:hover:border-gray-300 dark:text-white px-1 rounded-xs shadow-sm scheme-light dark:scheme-dark',
-        'ais-HitsPerPage-select':
-          'py-2 text-base dark:bg-black dark:text-white scheme-light dark:scheme-dark'
-      }"
-    />
+
+    <label>
+      <span class="sr-only">Results per page?</span>
+      <ais-hits-per-page
+        name="search-results-per-page"
+        :items="[
+          { label: '10 per page', value: 10, default: true },
+          { label: '25 per page', value: 25 },
+          { label: '50 per page', value: 50 },
+          { label: '100 per page', value: 100 }
+        ]"
+        :class-names="{
+          'ais-HitsPerPage':
+            'mt-6 md:mt-0 md:ml-4 px-2 bg-white text-base border border-gray-400 hover:border-black dark:bg-black dark:border-white dark:hover:border-gray-300 dark:text-white px-1 rounded-xs shadow-sm scheme-light dark:scheme-dark',
+          'ais-HitsPerPage-select':
+            'py-2 text-base dark:bg-black dark:text-white scheme-light dark:scheme-dark'
+        }"
+      />
+    </label>
+
   </div>
 </template>
 
