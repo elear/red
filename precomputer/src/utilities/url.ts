3a1e9cf94fb6933d617dd441df4d96707351967c
Matthew Holloway <matthew@staff.ietf.org>
2026-04-22 21:56:30 +1200

    feat: precomputer unpdf canvas fixes

--- precomputer/src/utilities/url.ts
+++ precomputer/src/utilities/url.ts
@@ -4,11 +4,7 @@ import type { RfcCommon, SubseriesCommon } from '../../../website/app/utilities/
 export const PUBLIC_SITE_URL_ORIGIN = 'https://www.rfc-editor.org'
 export const STAGING_SITE_URL_ORIGIN = 'https://www.staging.rfc-editor.org'
 export const apiRfcBucketDocumentURLBuilder = (fileName: string) => {
-  // Intentionally not a relative url, the PUBLIC_SITE prefix is because this URL is served
-  // from a bucket on prod; it's not something that a localhost Nuxt can serve.
-  // The CORS headers of the prod URL should allow access from localhost:3000 as well as staging,
-  // etc. sites.
-  return `${PUBLIC_SITE_URL_ORIGIN}/api/v1/rfc-html/${fileName}` as const
+  return `/api/v1/rfc-html/${fileName}` as const
 }
 
 export const infoRfcPathBuilder = (rfc: RfcCommon) => {
