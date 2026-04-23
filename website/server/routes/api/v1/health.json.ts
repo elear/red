3a060299c09a696b125ef14a13ba89cbd3f8c7ce
Matthew Holloway <matthew@staff.ietf.org>
2026-04-24 10:59:04 +1200

    fix: health check json

--- website/server/routes/api/v1/health.json.ts
+++ website/server/routes/api/v1/health.json.ts
@@ -20,15 +20,15 @@ export default defineEventHandler(async (event) => {
     '/about/rfc-editor/' // randomly chosen markdown page
   ]
 
-  const oks = await Promise.all(pathsToCheck.map(pathToCheck => httpOk(
+  const checks = await Promise.all(pathsToCheck.map(pathToCheck => httpCheck(
     new URL(pathToCheck, publicSiteUrlOrigin).toString()
   )))
 
-  const allOk = oks.every(ok => ok)
+  const allOk = checks.every(check => check.ok)
 
-  const failedPaths = oks.map((ok, index) => {
-    if (!ok) {
-      return pathsToCheck[index]
+  const failedPathsMessage = checks.map((check, index) => {
+    if (!check.ok) {
+      return `${pathsToCheck[index]} (HTTP ${check.statusCode})`
     }
     return undefined
   }).filter(Boolean).join(', ')
@@ -37,12 +37,15 @@ export default defineEventHandler(async (event) => {
 
   return {
     ok: allOk,
-    message: allOk ? undefined : `Health check failed on ${publicSiteUrlOrigin}: ${failedPaths}`,
+    message: allOk ? undefined : `Health check failed on ${publicSiteUrlOrigin}: ${failedPathsMessage}`,
   }
 })
 
-const httpOk = (url: string) => {
-  return new Promise<boolean>((resolve, reject) => {
+const httpCheck = (url: string) => {
+  return new Promise<{
+    ok: boolean,
+    statusCode: number
+  }>((resolve, reject) => {
     https
       .request(url, { method: 'HEAD' }, (res) => {
         const { statusCode } = res
@@ -50,10 +53,12 @@ const httpOk = (url: string) => {
           reject()
           return
         }
-        resolve(
+        resolve({
           // using fetch response 'ok' definition of ok
           // https://developer.mozilla.org/en-US/docs/Web/API/Response/ok
-          statusCode >= 200 && statusCode <= 299
+          ok: statusCode >= 200 && statusCode <= 299,
+          statusCode
+        }
         )
       }).on('error', (_err) => reject())
       .end()
