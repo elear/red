9b1591e6a6fdb7758fe321fdf481c416582349ef
Matthew Holloway <matthew@staff.ietf.org>
2026-05-08 23:02:30 +1200

    chore: canonical url security

--- worker/src/helpers.js
+++ worker/src/helpers.js
@@ -107,11 +107,11 @@ export function detectContentType(path) {
 
 function formatCanonicalHeader(url) {
   try {
-      const sanitisedUrl = new URL(url).toString() // can throw on invalid urls, which is preferable over adding an http header that might exploit some syntax quirk
+      const validatedUrl = new URL(url).toString() // can throw on invalid urls, which is preferable over adding an http header that might exploit some syntax quirk
       // canonical header is surrounded by '<' and '>' chars, so characters must be escaped.
       // encodeURI handles spaces, special characters, but not / : ? = &, so protocol prefix is left
       // untouched
-      const encodedUrl = encodeURI(sanitisedUrl)
+      const encodedUrl = encodeURI(validatedUrl)
         .replace(/</g, '%3C') // manually replace < and > just in case they are in the URL string
         .replace(/>/g, '%3E')
 
