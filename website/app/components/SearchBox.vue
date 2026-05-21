b7b674e29fb60a4bda936b47ba6f6cc0999c128a
Matthew Holloway <matthew@staff.ietf.org>
2026-05-22 10:44:34 +1200

    fix: preserve search query after js load

--- website/app/components/SearchBox.vue
+++ website/app/components/SearchBox.vue
@@ -1,6 +1,6 @@
 <template>
   <form method="get" :action="SEARCH_PATH" class="flex flex-row pt-4 pb-4 md:pb-8" @submit.stop.prevent="handleSearch">
-    <input id="search" v-model="searchQuery" type="search" name="q"
+    <input id="search" ref="search-input" v-model="searchQuery" type="search" name="q"
       class="min-w-[0px] w-full bg-white text-black dark:bg-black dark:text-white dark:border-white dark:border pl-4 md:pl-6 py-3"
       :placeholder="SEARCH_PLACEHOLDER" aria-label="Find an RFC (number, subseries, title, author, etc.)" />
     <button type="submit" name="search" class="bg-blue-200 px-2 flex items-center" aria-label="Submit search">
@@ -16,7 +16,9 @@ import { apiRfcBucketDocumentPathBuilder, apiSubseriesPathBuilder, infoSeriesPat
 
 const apiV1UrlOrigin = useApiV1UrlOrigin()
 
-const searchQuery = ref('')
+const searchInputRef = useTemplateRef('search-input')
+
+const searchQuery = ref(searchInputRef.value?.value ?? '')
 
 /**
  * If a user types something that looks like an RFC number or seriesId then just redirect to /info/*
@@ -24,7 +26,6 @@ const searchQuery = ref('')
  * But otherwise just redirect to search with url params etc.
  */
 const handleSearch = async () => {
-
   const { value } = searchQuery
   const normalizedValue = value.trim().replace(/\s/g, '')
   if (normalizedValue.match(/^[0-9]+$/)) {
