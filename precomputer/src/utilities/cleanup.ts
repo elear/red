03c8632213cfbeca8150805d641c9a58459931c0
Matthew Holloway <matthew@staff.ietf.org>
2026-05-25 17:59:56 +1200

    fix: make sure content files aren't purged from blobstore

--- precomputer/src/utilities/cleanup.ts
+++ precomputer/src/utilities/cleanup.ts
@@ -48,7 +48,8 @@ export const cleanupRedBucket = async (uploadedKeys: string[]): Promise<boolean>
       key.match(/^rfc\/\d+\.json$/) ||
       key.match(/^thumbnail\/rfc\d+\.png$/) ||
       key.match(/^thumbnail\/rfc-editor-logo-\d+x\d+\.png$/) ||
-      key.match(/^rfc\/\d+-page-\d+\.png$/) // pdf page screenshots
+      key.match(/^rfc\/\d+-page-\d+\.png$/) || // pdf page screenshots
+      key.match(/^content\/*.?\.json$/)
     ) {
       return false
     }
